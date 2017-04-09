e3b0ec6e9d4596077e31e9c07be7071f35626d25
HamzahHuailid <hamzahcatuas@gmail.com>
2017-04-09 20:22:11 +0700

    Add files via upload

---
+++ ViewData.java
@@ -0,0 +1,19 @@
+public class ViewData {
+	public void tulis (int x) {
+	System.out.println(x);
+	}
+	
+	public void tulis(double x) {
+		System.out.println(x);
+		}
+		
+	public void tulis(char x) {
+		System.out.println(x);
+		}
+		
+	public void tulis(String x) {
+		System.out.println(x);
+		}
+		
+	
+	}
---
+++ contoh_polymorphisme.java
@@ -0,0 +1,9 @@
+public class contoh_polymorphisme extends ViewData {
+		public static void main(String[] args) {
+			ViewData data = new ViewData() ;
+			data.tulis("Hamjah Huailid");
+			data.tulis("H");
+			data.tulis("10");
+			data.tulis("3.15");
+			}
+		}
